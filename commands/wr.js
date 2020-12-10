449130fed9039da0efe7ed8287338e9a32982f6d
Rektroth <53879295+Rektroth@users.noreply.github.com>
2020-12-10 18:44:26 -0500

    finished cleaning - need to implement

--- commands/wr.js
+++ commands/wr.js
@@ -3,6 +3,7 @@ module.exports = {
     description: "Gets the world record of a given category in a given game.",
     execute: async function(msg, args) {
         const discord = require("discord.js");
+        const fetch = require("node-fetch");
         const formatting = require("../formatting.js");
         var channelId = msg.channel.id;
 
@@ -16,26 +17,26 @@ module.exports = {
             var wrLink;
 
             var respGamesData = await fetch(`https://www.speedrun.com/api/v1/games?name=${game}`);
-            var gamesData = respGamesData.json();
+            var gamesData = await respGamesData.json();
 
             if (gamesData.data[0].names["international"] == game) {
                 var respCatData = await fetch(gamesData.data[0].links[3].uri);
-                var catData = respCatData.json();
+                var catData = await respCatData.json();
                 
                 if (catData.data.length != 0) {
                     for (let i = 0; i < catData.data.length; i++) {
-                        if (categoriesData.data[i].name == category) {
+                        if (catData.data[i].name == category) {
                             var respRecData = await fetch(catData.data[i].links[3].uri);
-                            var recData = respRecData.json();
+                            var recData = await respRecData.json();
 
-                            if (recordsData.data[0].runs.length != 0) {
+                            if (recData.data[0].runs.length != 0) {
                                 var respPlayerData = await fetch(recData.data[0].runs[0].run.players[0].uri);
-                                var playerData = respPlayerData.json();
+                                var playerData = await respPlayerData.json();
 
-                                wrTime = formatting.formatTime(recordsData.data[0].runs[0].run.times.primary_t);
+                                wrTime = formatting.formatTime(recData.data[0].runs[0].run.times.primary_t);
                                 wrHolder = playerData.data.names.international;
-                                wrDate = formatting.formatDate(recordsData.data[0].runs[0].run.date);
-                                wrLink = recordsData.data[0].runs[0].run.weblink;
+                                wrDate = formatting.formatDate(recData.data[0].runs[0].run.date);
+                                wrLink = recData.data[0].runs[0].run.weblink;
 
                                 msg.reply(new discord.MessageEmbed()
                                     .setColor(formatting.messageColor)
@@ -53,7 +54,7 @@ module.exports = {
                             break;
                         }
 
-                        if (i == categoriesData.data.length - 1) {
+                        if (i == catData.data.length - 1) {
                             msg.reply(new discord.MessageEmbed()
                                 .setColor(formatting.messageColor)
                                 .setTitle("Category Not Found")
